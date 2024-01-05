40befc6b434c67a8815c037fce0d6f8609ae8cf3
Muhammed Shafeeh <98475872+shafeehhecker@users.noreply.github.com>
2024-01-05 19:13:39 +0530

    Create CalcProd.java

---
+++ CalcProd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int CalcProd(int a,int b){
+        int sum = a*b;
+         return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        int sum = CalcProd(a,b);
+        System.out.println(sum);
+
+        }
+    }
