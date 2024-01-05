b8ffaecccc442a8c1e97155c8ffd912e89cfc090
Muhammed Shafeeh <98475872+shafeehhecker@users.noreply.github.com>
2024-01-05 19:21:30 +0530

    Create FAct.java
    A java program to calculate factorial ,using funtions

---
+++ FAct.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void Fact(int n){
+        int factorial =1;
+        for(int i=n;i>=1;i--) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        Fact(n);
+        }
+    }
